5af5f96551f7ba3b80690488c1a563040f751c09
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2020-05-21 12:08:56 +0530

    Add files via upload

--- pattern4.c
+++ pattern4.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n;
+	int n,i,j;
 	scanf("%d",&n);
-	for(i = 0;i < n;i++)
+	for(i = 0;i<n;i++)
 	{
-		for(j = 0;j< n;j++)
+		for(j = 0;j<=i;j++)
 		{
-			printf("%c ",65+i);
+			printf("*");
 		}
 		printf("\n");
+	
 	}
 }
