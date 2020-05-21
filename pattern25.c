362b4c8690447f8a667a2f051ecf9a34487d98e3
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2020-05-21 12:21:02 +0530

    Create pattern25.c

---
+++ pattern25.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,l;
+	scanf("%d",&n);
+for(i = 1;i<=n;i++)
+    {
+	for(j=n-i;j>0;j--)
+    {
+    	printf(" ");
+    }
+    
+    for(l = 1;l<=i;l++)
+   	{
+     printf("*");
+	}
+	printf("\n");	
+	     
+   }
+     
+    
+}
+
